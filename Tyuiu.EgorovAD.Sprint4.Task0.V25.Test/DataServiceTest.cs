36df83f57e0617864e3b283e8c91abd0aa82c1a6
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-11-11 21:22:20 +0300

    Add project files.

---
+++ Tyuiu.EgorovAD.Sprint4.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EgorovAD.Sprint4.Task0.V25.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task0.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 4, 2, 6, 4, 8, 6, 7, 8, 4 };
+            int res = ds.GetSumEvenArrEl(numsArray);
+            int wait = 38;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint4.Task0.V25/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.EgorovAD.Sprint4.Task0.V25.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task0.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] array = { 1, 4, 2, 6, 4, 8, 6, 7, 8, 4 };
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.GetSumEvenArrEl(array);
+
+
+
+            Console.WriteLine(res);
+            Console.ReadKey();  
+        }
+    }
+}

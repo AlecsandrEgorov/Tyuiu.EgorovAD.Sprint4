4f5273b3eb58d7fc6b9dc8746dd51156688ced12
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-11-12 19:30:55 +0300

    Task1

---
+++ Tyuiu.EgorovAD.Sprint4.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.EgorovAD.Sprint4.Task1.V7.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 1, 4, 2, 6, 4, 8, 6, 7, 8, 4 };
+            int res = ds.Calculate(numsArray);
+            int wait = 42;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint4.Task1.V7/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.EgorovAD.Sprint4.Task1.V7.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task1.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int len;
+            Console.WriteLine("Введите количество элементов массива:");
+            len = Convert.ToInt32(Console.ReadLine());  
+            int[] aray = new int[len];
+
+            for ( int i = 0; i < aray.Length; i++ )
+            {
+                Console.WriteLine("Введите значение " + i + " элемент массива");
+                aray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Массив:");
+            for ( int i = 0; i <=len - 1; i++ )
+            {
+                Console.WriteLine(aray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int res = ds.Calculate(aray);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}

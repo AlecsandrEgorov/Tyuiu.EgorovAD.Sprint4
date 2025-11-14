d60cf4d86272f46e09bfc8bbdf8f788d3e9ae04e
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-11-14 17:23:51 +0300

    Task6

---
+++ Tyuiu.EgorovAD.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.EgorovAD.Sprint4.Task6.V4.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string[] array = new string[] {"Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара"};
+
+            string[] res = ds.Calculate(array);
+
+            string[] wait = ["Москва", "Самара" ];
+
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint4.Task6.V4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Tracing;
+using Tyuiu.EgorovAD.Sprint4.Task6.V4.Lib;
+namespace Tyuiu.EgorovAD.Sprint4.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string[] cities = { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine(cities[i]);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] nums = ds.Calculate(cities);
+
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
